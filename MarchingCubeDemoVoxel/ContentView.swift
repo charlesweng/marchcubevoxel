1a0adcb35dc149d7c92ca12f2600c149cd686708
Charles Weng <hueborg@proton.me>
2024-10-18 17:47:59 -0700

    Initial Commit

---
+++ MarchingCubeDemoVoxel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarchingCubeDemoVoxel
+//
+//  Created by Charles Weng on 10/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
